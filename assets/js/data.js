d064ff0f8c675feaec9d2e9fbbe2ff7989a56bb2
Asadbek669 <akbarovasadbek777@gmail.com>
2026-02-08 13:34:51 +0300

    Update data.js: Jin Tilak tila

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,17 +1,7 @@
 const koreaDramas = [
   {
-    title: "Zolushka va to’rt Ritsar",
-    poster: "https://cdn.fastora.uz/Minxo_TV/IMG_20260205_200302_712.jpg",
-    link: "https://t.me/Minxotv_Arxiv/8464"
-  },
-  {
-    title: "Men bugundan insonman",
-    poster: "https://cdn.fastora.uz/Minxo_TV/IMG_20260205_200328_014.jpg",
-    link: "https://t.me/Minxotv_Arxiv/8430"
-  },
-  {
-    title: "Bahoriy hislar",
-    poster: "https://cdn.fastora.uz/Minxo_TV/IMG_20260205_210737_087.jpg",
-    link: "https://t.me/Minxotv_Arxiv/8386"
+    "title": "Jin Tilak tila",
+    "poster": "https://cdn.fastora.uz/yujin/jin-tilak-tila.jpg",
+    "link": "https://youdub.uz/jin-tilak-tila-uzbek-tilida"
   }
 ];
