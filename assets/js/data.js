508b9c386ae1a2beb6e5c44c253d1a40fb7a6425
Asadbek669 <akbarovasadbek777@gmail.com>
2026-02-08 13:41:35 +0300

    Update data.js: So'nggi yoz

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,5 +1,10 @@
 const koreaDramas = [
   {
+    "title": "So'nggi yoz",
+    "poster": "https://cdn.fastora.uz/yujin/songgi-yoz.jpg",
+    "link": "https://t.me/yujin_katalog/30635"
+  },
+  {
     "title": "Jin Tilak tila",
     "poster": "https://cdn.fastora.uz/yujin/jin-tilak-tila.jpg",
     "link": "https://youdub.uz/jin-tilak-tila-uzbek-tilida"
